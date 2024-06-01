f50f6fc7e90951731adc8ead433f7ff27f40b083
Kelvin <ctsang94@gmail.com>
2024-06-01 15:47:36 -0400

    Update VideoList Styling

--- src/component/VideoList/VideoList.jsx
+++ src/component/VideoList/VideoList.jsx
@@ -7,15 +7,17 @@ const VideoList = ({activeVideo, setActiveVideo, videoData}) => {
 
     return (
         <section className='video__list'>
-            <h2>NEXT VIDEOS</h2>
-            {videoData.map(video => 
+            <h2 className='video__next'>NEXT VIDEOS</h2>
+            {videoData.map(video =>
             <section onClick={()=> setActiveVideo(video)} className='video__container'>
-                <img className='video__img' src={video.image} />
-                <div className='video__details'>
-                {/* <div onClick={()=> clickHandler(video)} className='video__details'> */}
-                    <h3 className='video__title'>{video.title}</h3>
-                    <h4 className='video__channel'>{video.channel}</h4>
-                </div>
+                <article className='video__individual'>
+                    <img className='video__img' src={video.image} />
+                    <div className='video__details'>
+                    {/* <div onClick={()=> clickHandler(video)} className='video__details'> */}
+                        <h3 className='video__title'>{video.title}</h3>
+                        <h4 className='video__channel'>{video.channel}</h4>
+                    </div>
+                </article>
             </section>
             )}
         </section>
