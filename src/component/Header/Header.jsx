45f6b1bfd43e6e2b7ff1cf34a18a53ffb92bd0d3
Kelvin <ctsang94@gmail.com>
2024-06-01 18:11:12 -0400

    Update Header Tablet Styling

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -10,8 +10,9 @@ const Header = () => {
             <div className='header__mid'>
                 <input className='header__input' placeholder='Search'></input>
                 <img className='header__img' src={headerImage}></img>
-            </div>    
+            </div>
             <button className='header__btn'><img className='header__btn--icon' src={iconImage}></img>UPLOAD</button>
+            <img className='header__img--tablet' src={headerImage}></img>
         </header>
     )
 }
