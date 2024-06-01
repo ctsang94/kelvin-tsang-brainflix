8b7df2da01e51c3397fc6fa9a1fd656b9550712c
Kelvin <ctsang94@gmail.com>
2024-06-01 16:10:21 -0400

    Add Hide Functionality to Videolist

--- src/component/VideoList/VideoList.jsx
+++ src/component/VideoList/VideoList.jsx
@@ -1,19 +1,17 @@
 import './VideoList.scss'
 
 const VideoList = ({activeVideo, setActiveVideo, videoData}) => {
-    // const clickHandler = (video) => {
-    //     setActiveVideo(video)
-    // }
 
     return (
         <section className='video__list'>
             <h2 className='video__next'>NEXT VIDEOS</h2>
-            {videoData.map(video =>
-            <section onClick={()=> setActiveVideo(video)} className='video__container'>
+            {videoData
+            .filter(video => video.id !== activeVideo.id)
+            .map(video =>
+            <section key={video.id} onClick={()=> setActiveVideo(video)} className='video__container'>
                 <article className='video__individual'>
                     <img className='video__img' src={video.image} />
                     <div className='video__details'>
-                    {/* <div onClick={()=> clickHandler(video)} className='video__details'> */}
                         <h3 className='video__title'>{video.title}</h3>
                         <h4 className='video__channel'>{video.channel}</h4>
                     </div>
