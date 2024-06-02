ad587d0bc509f6c8a7f11efc05b6b7cec202b087
Kelvin <ctsang94@gmail.com>
2024-06-01 21:28:04 -0400

    Complete all styling. componentize videodetails

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import './styles/partials/global.scss'
 import Header from './component/Header/Header.jsx'
 import VideoPlayer from './component/VideoPlayer/VideoPlayer.jsx'
+import VideoDetails from './component/VideoDetails/VideoDetails.jsx'
 import Comments from "./component/Comments/Comments.jsx"
 import VideoList from './component/VideoList/VideoList.jsx'
 import videoData from './Data/video-details.json'
@@ -14,12 +15,17 @@ function App() {
     <>
       <Header />
       <VideoPlayer activeVideo={activeVideo}/>
-      <Comments activeVideo={activeVideo}/>
-      <VideoList
+      <div className='desktop__view'>
+        <div className='desktop__view--left'>
+          <VideoDetails activeVideo={activeVideo}/>
+          <Comments activeVideo={activeVideo}/>
+        </div>
+      <VideoList className='desktop__view--right'
         setActiveVideo={setActiveVideo}
         activeVideo={activeVideo}
         videoData={videoData}
       />
+      </div>
     </>
   )
 }
--- src/component/VideoPlayer/VideoPlayer.jsx
+++ src/component/VideoPlayer/VideoPlayer.jsx
@@ -1,5 +1,4 @@
 import './VideoPlayer.scss'
-import Video from '../../Data/video-details.json'
 
 const VideoPlayer = ({activeVideo}) => {
 
@@ -11,27 +10,8 @@ const VideoPlayer = ({activeVideo}) => {
         <video className='videoplayer' controls poster={activeVideo.image}>
             <source src={`${activeVideo.video}?api_key=kelvin`}></source>
         </video>
-
-        <section className='video'>
-            <h1 className='video__title'>{activeVideo.title}</h1>
-            <section className='video__stats'>
-                <article className='video__stats--top'>
-                    <h2 className='video__author'>By {activeVideo.channel}</h2>
-                    <img src='src\assets\Icons\views.svg'></img>
-                    <span className='video__views'>{activeVideo.views}</span>
-                </article>
-                <article className='video__stats--bottom'>
-                    <span className='video__date'>{date.toLocaleDateString()}</span>
-                    <img src='src\assets\Icons\likes.svg'></img>
-                    <span className='video__likes'>{activeVideo.likes}</span>
-                </article>
-            </section>
-            <p className='video__description'>{activeVideo.description}</p>
-        </section>
         </>
     )
 }
 
 export default VideoPlayer
-
-//80a98f3e-0d97-4fbd-9e61-23899b73cacb
