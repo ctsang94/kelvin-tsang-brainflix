2fb66000b21ca2b4af7a9d2923a75f0dc0fda574
Kelvin Tsang <ctsang94@gmail.com>
2024-06-06 00:26:08 -0400

    Add videos list via api call

--- src/component/VideoList/VideoList.jsx
+++ src/component/VideoList/VideoList.jsx
@@ -1,11 +1,31 @@
 import './VideoList.scss'
+import { useState } from 'react'
+import { useEffect } from 'react'
+import axios from 'axios'
 
 const VideoList = ({activeVideo, setActiveVideo, videoData}) => {
 
+    const baseUrl = 'https://unit-3-project-api-0a5620414506.herokuapp.com/'
+    const apiKey = '02a32350-3224-4f83-8c80-acd743ebc544'
+
+    const [videoList, setVideoList] = useState([])
+
+    const fetchVideos = async() =>{
+        const videos = await axios(`${baseUrl}videos?api_key=${apiKey}`)
+        console.log(videos)
+        setVideoList(videos.data)
+        console.log(videos.data)
+    }
+
+    useEffect(()=>{
+        fetchVideos();
+    }, [])
+
+    console.log(videoList)
     return (
         <section className='video__list'>
             <h2 className='video__next'>NEXT VIDEOS</h2>
-            {videoData
+            {videoList
             .filter(video => video.id !== activeVideo.id)
             .map(video =>
             <section key={video.id} onClick={()=> setActiveVideo(video)} className='video__container'>
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,13 +1,28 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import '../../styles/partials/global.scss'
 import VideoPlayer from "../../component/VideoPlayer/VideoPlayer";
 import VideoDetails from "../../component/VideoDetails/VideoDetails";
 import Comments from "../../component/Comments/Comments";
 import VideoList from "../../component/VideoList/VideoList";
 import videoData from '../../Data/video-details.json';
+import axios from "axios";
 
 const HomePage = () => {
+    const baseUrl = 'https://unit-3-project-api-0a5620414506.herokuapp.com/'
+    const apiKey = '02a32350-3224-4f83-8c80-acd743ebc544'
+
     const [activeVideo, setActiveVideo] = useState(videoData[0])
+
+    const fetchVideo = async() =>{
+        const  videos  = await axios(`${baseUrl}videos?api_key=${apiKey}`)
+        setActiveVideo(videos.data)
+        console.log(videos.data)
+    }
+
+    // useEffect(()=>{
+    //     fetchVideo();
+    // }, [])
+
     return (
         <>
             <VideoPlayer activeVideo={activeVideo} />
