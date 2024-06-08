81d9ccebe12bb6a368052b308d35d3853e59bf74
Kelvin <ctsang94@gmail.com>
2024-06-08 00:25:39 -0400

    Add link to brainflix logo and upload button

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -6,12 +6,12 @@ import iconImage from '../../assets/Icons/upload.svg'
 const Header = () => {
     return (
         <header className="header">
-            <img className='header__logo' src={logoImage}></img>
+            <a href='/'><img className='header__logo' src={logoImage}></img></a>
             <div className='header__mid'>
                 <input className='header__input' placeholder='Search'></input>
                 <img className='header__img' src={headerImage}></img>
             </div>
-            <button className='header__btn'><img className='header__btn--icon' src={iconImage}></img>UPLOAD</button>
+            <a href='/upload'><button className='header__btn'><img className='header__btn--icon' src={iconImage}></img>UPLOAD</button></a>
             <img className='header__img--tablet' src={headerImage}></img>
         </header>
     )
