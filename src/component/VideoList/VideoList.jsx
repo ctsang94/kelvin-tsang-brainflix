ab3dc08e3c1ac896ea38b35e9476cec58cda2d91
Kelvin <ctsang94@gmail.com>
2024-06-08 19:47:11 -0400

    Edit VideoList Scss nesting

--- src/component/VideoList/VideoList.jsx
+++ src/component/VideoList/VideoList.jsx
@@ -9,14 +9,15 @@ const VideoList = ({activeVideo, videoList}) => {
             {videoList
             .filter(video => video.id !== activeVideo.id)
             .map(video =>
-                <Link to={`/videos/${video.id}`}><section key={video.id} className='video__container'>
-                    <article className='video__individual'>
-                        <img className='video__img' src={video.image} alt='video-image'/>
-                        <div className='video__details'>
-                            <h3 className='video__title'>{video.title}</h3>
-                            <h4 className='video__channel'>{video.channel}</h4>
-                        </div>
-                    </article>
+                <Link to={`/videos/${video.id}`}>
+                    <section key={video.id} className='video__container'>
+                        <article className='video__individual'>
+                            <img className='video__img' src={video.image} alt='video-image'/>
+                            <div className='video__detail'>
+                                <h3 className='video__detail--title'>{video.title}</h3>
+                                <h4 className='video__detail--channel'>{video.channel}</h4>
+                            </div>
+                        </article>
                     </section>
                 </Link>
             )}
